6af1ccb6741c628c4dd0f8af88aebc18e8d34de9
Ben Lackey <ben.lackey@outlook.com>
2021-12-02 15:53:10 -0800

    platform image

--- validate.sh
+++ validate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation validate-template --template-body file://neo4j-gdb-enterprise.template
+aws cloudformation validate-template --template-body file://neo4j-gdb-enterprise.template.yaml
