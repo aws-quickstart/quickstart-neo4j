c87e841cfc9c58db3c945a5ce48baa2a6b40f35c
Ben Lackey <ben.lackey@neo4j.com>
2021-12-07 22:42:36 -0800

    add install script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ STACK_NAME=$1
 TEMPLATE_BODY="file://neo4j-gdb-enterprise.template.yaml"
 REGION=`aws configure get region`
 
-Password="foo123!"
+Password="foobar123"
 KeyName="neo4j-ben"
+
+echo "Creating stack $STACK_NAME"
 
 aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
