10b1479f889669b0f878624599605db88506ac37
Ben Lackey <ben.lackey@neo4j.com>
2021-12-08 16:28:36 -0800

    delete scratch

--- scratch.sh
+++
@@ -1,4 +0,0 @@
-
-coreMembers=$(aws autoscaling describe-auto-scaling-instances --region $region --output text --query "AutoScalingInstances[?AutoScalingGroupName=='Neo4jAutoScalingGroup'].InstanceId" | xargs -n1 aws ec2 describe-instances --instance-ids $ID --region $region --query "Reservations[].Instances[].PublicDnsName" --output text --filter "Name=tag:aws:cloudformation:stack-name,Values=$stackName")
-coreMembers=$(echo $coreMembers | sed 's/ /:5000,/g')
-coreMembers=$(echo $coreMembers):5000
